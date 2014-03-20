1188fd225c69aa18457d32f7b61a561890fee8bc
Adroaldo de Andrade <adroaldof@gmail.com>
2014-03-20 11:30:46 -0300

    Initial Commit

---
+++ Places/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Places
+//
+//  Created by Adro on 20/03/14.
+//  Copyright (c) 2014 Adro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
