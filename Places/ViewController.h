bca4d2df846087004aab147f012ad3ca2f0d3af7
Adroaldo de Andrade <adroaldof@gmail.com>
2014-03-22 14:45:11 -0300

    Add missing terminal file

---
+++ Places/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Places
+//
+//  Created by Adro on 22/03/14.
+//  Copyright (c) 2014 Adro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
