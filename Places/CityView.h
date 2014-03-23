4f6c6aa5cd453cbdabab1c77cf47db830c249de5
Adroaldo de Andrade <adroaldof@gmail.com>
2014-03-22 21:23:10 -0300

    Add city view with its labels

---
+++ Places/CityView.h
@@ -0,0 +1,17 @@
+//
+//  CityView.h
+//  Places
+//
+//  Created by Adro on 21/03/14.
+//  Copyright (c) 2014 Adro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CityView : UIView
+
+@property (strong, nonatomic) UILabel *nameLabel;
+@property (strong, nonatomic) UILabel *stateLabel;
+@property (strong, nonatomic) UILabel *populationLabel;
+
+@end
