f85f26f13fe7fa3f336695eafcad8afd71dfe8e8
Adroaldo de Andrade <adroaldof@gmail.com>
2014-03-24 12:24:09 -0300

    Add places table view controller

---
+++ Places/PlaceTableVC.h
@@ -0,0 +1,16 @@
+//
+//  PlaceTableVC.h
+//  Places
+//
+//  Created by Adro on 24/03/14.
+//  Copyright (c) 2014 Adro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaceTableVC : UITableViewController
+
+@property (strong, nonatomic) NSArray *places;
+@property (strong, nonatomic) NSArray *descriptions;
+
+@end
