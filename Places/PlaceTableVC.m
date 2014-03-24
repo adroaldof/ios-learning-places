7d070497ad9cd64a9f57d318f05bbb8fc7aeaeee
Adroaldo de Andrade <adroaldof@gmail.com>
2014-03-24 17:12:46 -0300

    Request places implementation
    A detail must be noted, first initial data is set to a mutable array and after fetch all data on it the places property is initialized by casting that mutable array in an array.

--- Places/PlaceTableVC.m
+++ Places/PlaceTableVC.m
@@ -6,7 +6,9 @@
 //  Copyright (c) 2014 Adro. All rights reserved.
 //
 
+#import "Place.h"
 #import "PlaceTableVC.h"
+#import "AFJSONRequestOperation.h"
 
 @interface PlaceTableVC ()
 
@@ -27,6 +29,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    NSURL *url = [[NSURL alloc] initWithString:@"http://localhost:8080/Places/get/places"];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    AFJSONRequestOperation *opearion = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+
+        NSMutableArray *tempArray = [[NSMutableArray alloc] init];
+
+        for (NSDictionary *internalDic in JSON) {
+            Place *newPlace = [[Place alloc] initWithName:internalDic[@"name"] description:internalDic[@"description"]];
+            [tempArray addObject:newPlace];
+        }
+
+        self.places = [[NSArray alloc] initWithArray:tempArray];
+
+        [self.tableView reloadData];
+
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        NSLog(@"Error: %@", error.localizedDescription);
+    }];
+
+    [opearion start];
 
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
