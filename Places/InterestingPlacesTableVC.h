e7011478133246ca615a6e991a30c4559a19a42f
Adroaldo de Andrade <adroaldof@gmail.com>
2014-03-31 11:19:03 -0300

    Add initial interesting places table view controller

---
+++ Places/InterestingPlacesTableVC.h
@@ -0,0 +1,13 @@
+//
+//  InterestingPlacesTableVC.h
+//  Places
+//
+//  Created by Adro on 31/03/14.
+//  Copyright (c) 2014 Adro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InterestingPlacesTableVC : UITableViewController
+
+@end
